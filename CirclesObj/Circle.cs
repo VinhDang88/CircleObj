fa7b21bc0d945619c062066caa6bb9477ce11997
VinhDang88 <vincedang720@gmail.com>
2022-07-06 06:45:14 -0400

    First repo

---
+++ CirclesObj/Circle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CirclesObj
+{
+    internal class Circle
+    {
+        //properties
+
+        public double Radius;
+
+        //constructors 
+        public Circle(double radius)
+        {
+            Radius = radius;
+
+        }
+        //methods
+        public double CalculateCircumference()
+        {
+            double circumference = 0;
+            //double circumference2 = 0;
+            return circumference = Math.PI * Radius * 2;
+            //return circumference2 = Math.Round(circumference, 2);
+        }
+        public string CalculateFormattedCircumference()
+        {
+            //CalculateCircumference();
+            string circumference = ($"circumference: {FormatNumber(CalculateCircumference())}");
+            return circumference;
+        }
+        public double CalculateArea()
+        {
+            double area = 0;
+            //double area2 = 0;
+            return area = Math.PI * Radius * Radius;
+            //return area2 = Math.Round(area, 2);
+        }
+        public string CalculateFormattedArea()
+        {
+            string area = ($"area: {FormatNumber(CalculateArea())}");
+            return area;
+
+        }
+        private string FormatNumber(double x)
+        {
+            return Math.Round(x, 2).ToString(); //old lab bring string as a string datatype.
+        }
+
+    }
+}
---
+++ CirclesObj/Program.cs
@@ -0,0 +1,76 @@
+﻿using CirclesObj;
+bool runProgram = true;
+bool computeData = true;
+int count = 0;
+Console.WriteLine("Welcome to the Circle Tester");
+
+
+//double input = double.Parse(Console.ReadLine());
+while (runProgram)
+{
+    while (computeData)
+    {
+
+        try
+        {
+            Console.WriteLine("Enter radius:");
+
+            double input = double.Parse(Console.ReadLine());
+            if (input <= 0)
+            {
+                Console.WriteLine("Enter a number greater than 0");
+            }
+
+            else
+            {
+                Circle myCircle = new Circle(input);
+                //myCircle.CalculateCircumference();
+                //myCircle.CalculateArea();
+                Console.WriteLine(myCircle.CalculateFormattedCircumference());
+                Console.WriteLine(myCircle.CalculateFormattedArea());
+                count++;
+                //  private string FormatNumber(double x) am i suppose to put in the varialbe 'input' inside this parameter?? Not sure.
+                break;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+
+        }
+    }
+    while (true)
+    {
+        Console.WriteLine("Go again? (y/n)");
+        string playAgain = Console.ReadLine().ToLower().Trim();
+        if (playAgain == "y")
+        {
+            break;
+        }
+        else if (playAgain == "n")
+        {
+            Console.WriteLine($"You calculated {count} circles.");
+            Console.WriteLine("Come back again friend!");
+            
+            runProgram = false;
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you entered a bad input.");
+            continue;
+        }
+    }
+
+
+}
+
+
+//Define a member called radius. This member should be private. Not sure why I want radius to be private?
+//Continue run program later.
+
+//Circle myCircle = new Circle(input);
+//myCircle.CalculateCircumference();
+//myCircle.CalculateArea();
+//Console.WriteLine(myCircle.CalculateFormattedCircumference()); 
+//Console.WriteLine(myCircle.CalculateFormattedArea()); 
